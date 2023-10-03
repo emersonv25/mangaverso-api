38d3aaa15eb6157a8e49694d28c2182fc88f8c75
emersonv25 <emersondejesussantos@hotmail.com>
2023-10-03 15:12:30 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+  
   const config = new DocumentBuilder()
     .setTitle('MangaVerso Api')
     .setDescription('The Manga Verso API description')
