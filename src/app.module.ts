e604822a31625276652aec71795ecfc8e8d6e56e
emersonv25 <emersondejesussantos@hotmail.com>
2023-10-17 22:25:23 -0300

    Hello World no index

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MangaModule } from './manga/manga.module';
 import { ChapterModule } from './chapter/chapter.module';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [ConfigModule.forRoot(), MangaModule, ChapterModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
